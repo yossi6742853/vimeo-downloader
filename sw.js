38f792cf3fdee4c170ab726fbbef3cef763ebd2f
Yosef Schneider <6742853@gmail.com>
2026-04-27 19:26:19 +0300

    V4: All file types, dark mode, PWA, push notifications, Drive browser prep

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker for PWA
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
